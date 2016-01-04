2205e8b7babc6b18ca246ddc3a83b48df84b809c
Jose Añasco <joseanasco1@gmail.com>
2016-01-04 15:31:00 -0500

    extract js object reflection/creation functionality

--- opal/wrappable.rb
+++ opal/wrappable.rb
@@ -3,40 +3,24 @@ require 'js'
 
 require 'active_support/core_ext/string'
 
+require_relative 'wrappable/js_object'
+
 module Wrappable
   def self.included(klass)
     klass.class_eval {
       include Native
 
-      # NOTE: identifies methods that start with underscore as private methods
-      js_properties = (JSHelpers.object_properties(JSHelpers.window(klass).JS[:prototype]) - %w(constructor)).reject { |method| method.start_with? '_' }
+      js_object = JSObject.new(klass)
 
-      js_properties.each do |js_method|
+      js_object.properties(:methods).each do |js_method|
         alias_native(js_method.underscore, js_method)
       end
 
-      # TODO: this is duplicating method creation, extract-refactor
-      attributes = JSHelpers.object_properties(JSHelpers.constructor klass)
-
-      native_accessor *attributes
+      native_accessor *(js_object.properties :attributes)
 
       def initialize(*args)
-        super JSHelpers.constructor(self.class, args)
+        super JSObject.new(self.class).construct(args)
       end
     }
-  end
-
-  module JSHelpers
-    def self.constructor(property, args = [])
-      JS.new(window(property), args)
-    end
-
-    def self.object_properties(object)
-      Array(`Object`.JS.getOwnPropertyNames(object))
-    end
-
-    def self.window(property)
-      `window`.JS[property]
-    end
   end
 end
---
+++ opal/wrappable/js_object.rb
@@ -0,0 +1,37 @@
+module Wrappable
+  class JSObject
+    def initialize(name)
+      @name = name
+    end
+
+    def construct(args = [])
+      JS.new(obtain, args)
+    end
+
+    def properties(type)
+      if type == :methods
+        # NOTE: identifies methods that start with underscore as private methods
+        (property_names(prototype) - %w(constructor))
+          .reject { |method| method.start_with? '_' }
+      elsif type == :attributes
+        Array(property_names construct)
+      else
+        fail 'incorrect type'
+      end
+    end
+
+    private
+
+    def property_names(object)
+      `Object`.JS.getOwnPropertyNames object
+    end
+
+    def prototype
+      obtain.JS[:prototype]
+    end
+
+    def obtain
+      `window`.JS[@name]
+    end
+  end
+end
