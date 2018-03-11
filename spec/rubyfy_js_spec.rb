28021ac5814b517c3d57205797c82e953b11b52c
Jose Anasco <joseanasco1@gmail.com>
2018-03-11 11:38:37 -0400

    add modules on tests

--- spec/rubyfy_js_spec.rb
+++ spec/rubyfy_js_spec.rb
@@ -55,30 +55,32 @@ describe RubyfyJS do
 
   describe 'accesors for object types' do
     before {
-      class Lion
-        include RubyfyJS
-      end
+      module Africa
+        class Lion
+          include RubyfyJS
+        end
 
-      class Puma
-        include RubyfyJS
-      end
+        class Puma
+          include RubyfyJS
+        end
 
-      class Zebra
-        include Native
-      end
+        class Zebra
+          include Native
+        end
 
-      class Jungle
-        include RubyfyJS
+        class Jungle
+          include RubyfyJS
+        end
       end
     }
 
     it 'wraps object attributes' do
-      expect(Jungle.new.big_lion).to be_a Lion
-      expect(Jungle.new.little_puma).to be_a Puma
-      expect(Jungle.new.little_zebra).to be_a Zebra
+      expect(Africa::Jungle.new.big_lion).to be_a Africa::Lion
+      expect(Africa::Jungle.new.little_puma).to be_a Africa::Puma
+      expect(Africa::Jungle.new.little_zebra).to be_a Africa::Zebra
 
-      expect(Jungle.new.big_lion.age).to eq 7
-      expect(Jungle.new.big_lion.say_roar).to eq 'Roar!!!, im the king of the jungle'
+      expect(Africa::Jungle.new.big_lion.age).to eq 7
+      expect(Africa::Jungle.new.big_lion.say_roar).to eq 'Roar!!!, im the king of the jungle'
     end
   end
 end
